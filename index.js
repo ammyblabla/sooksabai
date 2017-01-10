ce343d3b52935307d1fe25807929c03eaec83bf8
ammyblabla <you@example.com>
2017-01-10 21:05:17 +0700

    text content finished

--- index.js
+++ index.js
@@ -1,4 +1,14 @@
-$(document).ready(function(){
+$(document).ready(function() {
   $('.scrollspy').scrollSpy();
   $('.parallax').parallax();
+  $('.button-collapse').sideNav();
+  $('.collapsible').collapsible();
 });
+
+$('.button-collapse').sideNav({
+    menuWidth: 300, // Default is 240
+    edge: 'right', // Choose the horizontal origin
+    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+    draggable: true // Choose whether you can drag to open on touch screens
+  }
+);
