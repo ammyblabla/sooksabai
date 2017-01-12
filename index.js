cc6f598f6d140a7a37f58e69f01f6fb3d6f01bbd
ammyblabla <you@example.com>
2017-01-12 23:30:03 +0700

    add hamburger button for SideNav

--- index.js
+++ index.js
@@ -6,14 +6,12 @@ $(document).ready(function() {
   $('.carousel').carousel({
     indicators:true
   });
+  $('.button-collapse').sideNav({
+      menuWidth: 200, // Default is 240
+      // edge: 'right', // Choose the horizontal origin
+      closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
+      draggable: true // Choose whether you can drag to open on touch screens
+    }
+  );
+  $('.carousel.carousel-slider').carousel({full_width: true});
 });
-
-$('.button-collapse').sideNav({
-    menuWidth: 300, // Default is 240
-    edge: 'right', // Choose the horizontal origin
-    closeOnClick: true, // Closes side-nav on <a> clicks, useful for Angular/Meteor
-    draggable: true // Choose whether you can drag to open on touch screens
-  }
-);
-
-$('.carousel.carousel-slider').carousel({full_width: true});
